e37d058e9c3f0b1ff3f4fa00c8d008c578043de9
zhanyany16 <zhanyany@amazon.com>
2024-10-04 04:05:08 +0000

    Update clean up code

--- langgraph_implementation/dependencies/clean.py
+++ langgraph_implementation/dependencies/clean.py
@@ -23,15 +23,16 @@ try:
     if 'Contents' in objects:
         for obj in objects['Contents']:
             s3_client.delete_object(Bucket=param_dict['S3BucketName'], Key=obj['Key'])
-    print(f"Bucket '{param_dict['S3BucketName']}' empty successfully.")
 except:
     pass
 
 aoss_collection_id = param_dict['AOSSHost'].split('.')[0]
 
 # Delete Collection for OpenSearchServerless
-aoss_client.delete_collection(
-    id=aoss_collection_id
-)
-print(f"Collection '{param_dict['AOSSCollectionName']}' deleted successfully.")
+try:
+    aoss_client.delete_collection(
+        id=aoss_collection_id
+    )
+except:
+    pass
 
